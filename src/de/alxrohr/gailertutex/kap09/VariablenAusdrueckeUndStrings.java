35ff53a349fa86d1fdfb9d659ad4c2d9c9876b94
Alexander Rohr <a.rohr@web.de>
2012-04-10 22:41:52 +0200

    Weitere Aufgaben zu Kapitel 10

--- src/de/alxrohr/gailertutex/kap09/VariablenAusdrueckeUndStrings.java
+++ src/de/alxrohr/gailertutex/kap09/VariablenAusdrueckeUndStrings.java
@@ -3,16 +3,13 @@
  */
 package de.alxrohr.gailertutex.kap09;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  * @author Alexander Rohr
  * 
  * Übungen zu Kapitel 9: Variablen, Operationen, Objekte
  *
  */
-public class VariablenAusdrückeUndStrings {
+public class VariablenAusdrueckeUndStrings {
 	public final static double MWST = 0.19; // eine Konstante
 
 	public static void main ( String[] args ) {
@@ -24,13 +21,28 @@ public class VariablenAusdrückeUndStrings {
 
 		System.out.println("Die Vorzeichen-Operatoren funktionieren auch für Variablen: -x ist " + -x + " und +-+y ist " + +-+y);
 	
-		String str = new String("Elementary, dear Watson!");
+		String str1, str = new String("Elementary, dear Watson!");
 		System.out.println("Die Länge von str ist " + str.length() );
 
+		System.out.println( "Die ersten neun Zeichen: " + str.substring(0, 9) );
+		System.out.println( "Das neunte Zeichen: " + str.substring(9, 9) );
+		System.out.println( "Die ersten 24 Zeichen: " + str.substring(0, 24) );
+		System.out.println( "Alle Zeichen: " + str.substring(0, str.length() ) );
+		System.out.println( "Alle Zeichen ab dem 11ten: " + str.substring(10, str.length() ) );
+		// System.out.println( "Ein Zeichen zuviel: " + str.substring(0, 25) );
+		// System.out.println( "Vertauschte Grenzen: " + str.substring(10, 9) );
+		
 		// Eine sehr flüchtige Instanz eines String-Objekts, sozusagen ein Einweg-Objekt
 		System.out.println( new String("Elementar").length() );
 
 		// Noch ein Einweg-Objekt
 		System.out.println( str.substring(12) );
+		
+		// System.out.println("Eine Stringvariable ohne Inhalt: " + str1);
+		
+		str1 = "   Foo\t\tbar\t\t\n";
+		System.out.println( "xxx" + str1 + "xxx" );
+		System.out.println( "xxx" + str1.trim() + "xxx" );
+		
 	}
 }
---
+++ src/de/alxrohr/gailertutex/kap10/KreisFlaeche.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.alxrohr.gailertutex.kap10;
+
+import java.util.Scanner;
+
+/**
+ * @author Alexander Rohr
+ *
+ */
+public class KreisFlaeche {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println( "Dieses Programm berechnet die Kreisfläche aus dem Radius." );
+
+		System.out.println( "Bitte einen ganzzahligen Radius eingeben: " );
+		double radius = input.nextInt();
+
+		double flaeche = 2 * Math.PI * radius;
+
+		System.out.println( "Der Kreis hat eine Fläche von " + flaeche );
+	}
+
+}
